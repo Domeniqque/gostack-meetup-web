ca4d4292e402cf95a0220e94b8697e300778bf22
Domeniqque Dylluar <domeniqque@hotmail.com>
2019-10-12 18:57:39 -0400

    chore: add initial path settings

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function SignIn() {
+  return (
+    <div>
+      <h3>Signin</h3>
+    </div>
+  );
+}
---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function SignUp() {
+  return (
+    <div>
+      <h3>SignUp</h3>
+    </div>
+  );
+}
